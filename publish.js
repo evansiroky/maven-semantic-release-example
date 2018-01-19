41b03060c244d0967546d6f8acf2ada20c13380b
Travis CI <travis@travis-ci.org>
2018-01-19 15:37:01 -0800

    work in progress on publishing to maven, git

--- publish.js
+++ publish.js
@@ -2,26 +2,69 @@ const execa = require('execa')
 const fs = require('fs-extra')
 const SemanticReleaseError = require('@semantic-release/error')
 
-module.exports = async function (pluginConfig, {options, logger, lastRelease, commits, nextRelease}) {
-  // set version number in pom.xml
-  await setVersionInPomXml(nextRelease.version)
+const config = require('./config.json')
+
+module.exports = publish
+
+publish({}, {options: {}, logger: console, nextRelease: { version: '1.0.0' } })
+
+async function publish (pluginConfig, {options, logger, lastRelease, commits, nextRelease}) {
+  // configure git to allow pushing
+  await configureGit(logger)
+
+  // set and commit version number in pom.xml
+  await commitVersionInPomXml(nextRelease.version, logger)
 
   // publish to maven
-  logger.log('Publishing version %s to npm registry', nextRelease.version);
-  const shell = await execa('mvn', ['deploy']);
-  process.stdout.write(shell.stdout);
+  logger.log('Deploying version %s with maven', nextRelease.version);
+  try {
+    const shell = await execa('mvn', ['deploy']);
+    process.stdout.write(shell.stdout);
+  } catch (e) {
+    throw new SemanticReleaseError('failed to deploy to maven')
+  }
 
   // update version to next snapshot version
   const nextSnapshotVersion = nextRelease.version.split('.').map(s => parseInt(s, 10))
   nextSnapshotVersion[2] += 1
-  await setVersionInPomXml(`${nextSnapshotVersion.join('.')}-SNAPSHOT`)
+  await commitVersionInPomXml(`${nextSnapshotVersion.join('.')}-SNAPSHOT`, logger)
 
   // make a commit bumping to snapshot version
 }
 
-setVersionInPomXml('1.0.0', console)
+/**
+ * Configure git settings.  Copied from this guide: https://gist.github.com/willprice/e07efd73fb7f13f917ea
+ */
+async function configureGit (logger) {
+  let shell = await execa(
+    'git',
+    ['config', '--global', 'user.email', '"travis@travis-ci.org"']
+  )
+  process.stdout.write(shell.stdout)
+  shell = await execa(
+    'git',
+    ['config', '--global', 'user.name', '"Travis CI"']
+  )
+  process.stdout.write(shell.stdout)
+  try {
+    shell = await execa(
+      'git',
+      [
+        'remote',
+        'add',
+        'origin',
+        `https://${config.GH_TOKEN}@github.com/evansiroky/resources.git`
+      ]
+    )
+    process.stdout.write(shell.stdout)
+  } catch (e) {
+    if (!(e.message.indexOf('remote origin already exists') > -1)) {
+      throw e
+    }
+  }
+}
 
-async function setVersionInPomXml (versionStr, logger) {
+async function commitVersionInPomXml (versionStr, logger) {
   let pomLines
   try {
     pomLines = (await fs.readFile('./pom.xml', 'utf8')).split('\n')
@@ -42,4 +85,19 @@ async function setVersionInPomXml (versionStr, logger) {
   }
 
   await fs.writeFile('./pom.xml', pomLines.join('\n'))
+
+  const commitMessage = versionStr.indexOf('SNAPSHOT') > -1
+    ? `Prepare next development iteration ${versionStr}`
+    : versionStr
+
+  logger.log('adding pom.xml to a commmit')
+  let shell = await execa('git', ['add', 'pom.xml'])
+  process.stdout.write(shell.stdout)
+
+  logger.log('committing changes')
+  shell = await execa('git', ['commit', '-m', commitMessage])
+  process.stdout.write(shell.stdout)
+  process.stdout.write('\n')
+
+  logger.log('changes committed')
 }
