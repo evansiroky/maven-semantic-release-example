e6269ef3a8e4f743db4def9ad8c45f763011b608
evansiroky <evan.siroky@yahoo.com>
2018-01-25 20:04:34 -0800

    make git push dangerously verbose
    I wanna see what’s happening on Travis

--- push.sh
+++ push.sh
@@ -7,7 +7,7 @@ if [[ $GH_TOKEN ]]; then
     remote=https://$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG
 fi
 
-if ! git push --quiet "$remote" "$TRAVIS_BRANCH" > /dev/null 2>&1; then
+if ! git push "$remote" "$TRAVIS_BRANCH" ; then
     err "failed to push git changes"
     return 1
 fi
