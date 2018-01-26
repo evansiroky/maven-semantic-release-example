e4742b4723909e767988204e967345410f34f420
evansiroky <evan.siroky@yahoo.com>
2018-01-25 20:28:09 -0800

    echo git push command
    I’m gonna delete the token right after this

--- push.sh
+++ push.sh
@@ -7,7 +7,10 @@ if [[ $GH_TOKEN ]]; then
     remote=https://$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG
 fi
 
+echo "bash pushing"
+echo "git push $remote $TRAVIS_BRANCH"
 if ! git push "$remote" "$TRAVIS_BRANCH" ; then
     err "failed to push git changes"
     return 1
 fi
+echo "done pushing"
